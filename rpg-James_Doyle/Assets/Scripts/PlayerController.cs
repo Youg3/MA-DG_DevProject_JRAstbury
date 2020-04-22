76731a56f5bfae56aba5897d3271d7e209edf81c
Joshua Astbury <j.astbury@ymail.com>
2020-04-22 16:22:11 +0200

    Anim, PlayerMovement, SpriteSheet
    Following tutorial until video 13

---
+++ rpg-James_Doyle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    //vars
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"))*moveSpeed;
+    }
+}
