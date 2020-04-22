c4ef851544858b96ce2b279d1a8ee5b261bab9a7
Joshua Astbury <j.astbury@ymail.com>
2020-04-22 17:17:44 +0200

    Switching Scenes, Managing Player, unpacked add. assets
    Vid 16 completed, imported the additional packaged assets, adapted player controller script to allow only 1 obj, new test2 scene added

---
+++ rpg-James_Doyle/Assets/Scripts/AreaExit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    //vars
+    public string areaToLoad;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(areaToLoad);
+        }
+    }
+
+}
--- rpg-James_Doyle/Assets/Scripts/PlayerController.cs
+++ rpg-James_Doyle/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,44 @@ public class PlayerController : MonoBehaviour
 {
     //vars
     public Rigidbody2D theRB;
+    public Animator playerAnim;
     public float moveSpeed;
+
+    public static PlayerController instance;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //checks that this is the only player in scene
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            //destroy new player object
+            Destroy(gameObject);
+        }
+
+        //don't destroy player on changing scenes
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"))*moveSpeed;
+
+        playerAnim.SetFloat("moveX", theRB.velocity.x);
+        playerAnim.SetFloat("moveY", theRB.velocity.y);
+
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            playerAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            playerAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
     }
 }
