12273960059c58f3dbe7e2e7e64632deb84f9296
Joshua Astbury <j.astbury@ymail.com>
2020-04-23 13:12:03 +0200

    Player Prefab, Player Loader Script, Camera Controller Script
    Following tutorials to implement heading scripts to game

---
+++ rpg-James_Doyle/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //ref to obj camera focuses on
+    public Transform target;
+
+    void Start()
+    {
+        //gets the transform positions of the player
+        target = PlayerController.instance.transform;
+    }
+
+    //LateUpdate calls AFTER other Update scripts
+    void LateUpdate()
+    {
+        //update the camera every frame
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
