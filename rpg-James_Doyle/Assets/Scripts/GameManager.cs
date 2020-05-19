620dbbafd12b9a294387048e4caeae33d121f811
Joshua Astbury <j.astbury@ymail.com>
2020-05-19 12:24:46 +0200

    Videos: 77 - 79; Section 12 Saving Complete
    Completed section 12 and added saving of gold amounts and loading of both gold and scene.

--- rpg-James_Doyle/Assets/Scripts/GameManager.cs
+++ rpg-James_Doyle/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -48,6 +50,9 @@ public class GameManager : MonoBehaviour
             RemoveItem("HP Potion");
             RemoveItem("Bleep");
         }
+
+        if (Input.GetKeyDown(KeyCode.O)) { SaveData(); }
+        if (Input.GetKeyDown(KeyCode.P)) { LoadData(); }
     }
 
     public Item GetItemDetails(string itemToGrab)
@@ -175,5 +180,98 @@ public class GameManager : MonoBehaviour
             }
         }
         return false;
+    }
+
+    public void SaveData()
+    {
+        //save scene
+        PlayerPrefs.SetString("Current_Scene_", SceneManager.GetActiveScene().name);
+
+        //save where the player is in the world
+        PlayerPrefs.SetFloat("Player_Position_x", PlayerController.instance.transform.position.x);
+        PlayerPrefs.SetFloat("Player_Position_y", PlayerController.instance.transform.position.y);
+        PlayerPrefs.SetFloat("Player_Position_z", PlayerController.instance.transform.position.z);
+
+        //char details/info
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy) //checks to see which char's are active in the game
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_active",1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_active",0);
+            }
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_Level", playerStats[i].playerLevel); //saves char level
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_currentHP", playerStats[i].currentHP); //saves char current HP
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_maxHP", playerStats[i].maxHP); //saves char current HP
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_currentMP", playerStats[i].currentMP); //saves char current MP
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_maxMP", playerStats[i].maxMP); //saves char current MP
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_currentEXP", playerStats[i].currentEXP); //saves char current EXP
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_Strength", playerStats[i].strength); //saves char current STR
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_Defense", playerStats[i].defense); //saves char current DEF
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_WpnPwr", playerStats[i].weaponPower); //saves char current Weapon Power
+            PlayerPrefs.SetInt("Player_" + playerStats[i].charName + "_ArmPwr", playerStats[i].armourPower); //saves char current Armour Power
+            PlayerPrefs.SetString("Player_" + playerStats[i].charName + "_Weapon", playerStats[i].weaponName); //saves char current Equipped Weapon
+            PlayerPrefs.SetString("Player_" + playerStats[i].charName + "_Armour", playerStats[i].armourName); //saves char current Equipped Armour
+            
+        }
+
+        //store Inventory Data
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            PlayerPrefs.SetString("Item_In_Inventory_" + i, itemsHeld[i]); //stores item names
+            PlayerPrefs.SetInt("Item_Amount_" + i, numberOfItems[i]); //stores amount of items
+        }
+
+        //store gold amount
+        PlayerPrefs.SetInt("Gold_Amount_", currentGold);
+
+    }
+
+    public void LoadData()
+    {
+        //load scene
+        SceneManager.LoadScene(PlayerPrefs.GetString("Current_Scene_"));
+        //loads player position
+        PlayerController.instance.transform.position = new Vector3(PlayerPrefs.GetFloat("Player_Position_x"), PlayerPrefs.GetFloat("Player_Position_y"), PlayerPrefs.GetFloat("Player_Position_z"));
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if (PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_active") == 0)
+            {
+                playerStats[i].gameObject.SetActive(false); //inactive player/char
+            }
+            else
+            {
+                playerStats[i].gameObject.SetActive(true); //should be active player/char
+            }
+
+            playerStats[i].playerLevel = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_Level"); //get char level
+            playerStats[i].currentHP = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_currentHP"); //saves char current HP
+            playerStats[i].maxHP = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_maxHP"); //saves char current HP
+            playerStats[i].currentMP = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_currentMP"); //saves char current MP
+            playerStats[i].maxMP = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_maxMP"); //saves char current MP
+            playerStats[i].currentEXP = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_currentEXP"); //saves char current EXP
+            playerStats[i].strength = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_Strength"); //saves char current STR
+            playerStats[i].defense = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_Defense"); //saves char current DEF
+            playerStats[i].weaponPower = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_WpnPwr"); //saves char current Weapon Power
+            playerStats[i].armourPower = PlayerPrefs.GetInt("Player_" + playerStats[i].charName + "_ArmPwr"); //saves char current Armour Power
+            playerStats[i].weaponName = PlayerPrefs.GetString("Player_" + playerStats[i].charName + "_Weapon"); //saves char current Equipped Weapon
+            playerStats[i].armourName = PlayerPrefs.GetString("Player_" + playerStats[i].charName + "_Armour"); //saves char current Equipped Armour
+        }
+
+        //inventory
+        for (int i = 0; i < itemsHeld.Length; i++)
+        {
+            itemsHeld[i] = PlayerPrefs.GetString("Item_In_Inventory_" + i);
+            numberOfItems[i] = PlayerPrefs.GetInt("Item_Amount_" + i);
+        }
+
+        //load gold amount
+        currentGold = PlayerPrefs.GetInt("Gold_Amount_");
+
     }
 }
--- rpg-James_Doyle/Assets/Scripts/GameMenu.cs
+++ rpg-James_Doyle/Assets/Scripts/GameMenu.cs
@@ -256,4 +256,10 @@ public class GameMenu : MonoBehaviour
         activeItem.Use(selectChar);
         CloseItemCharChoice();
     }
+
+    public void SaveGame()
+    {
+        GameManager.instance.SaveData();
+        QuestManager.instance.SaveQuestData();
+    }
 }
