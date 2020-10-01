9b9fe547abc08fe75a6b39511c27efeb1290c47a
Joshua Astbury <j.astbury@ymail.com>
2020-10-01 12:54:59 +0200

    Refactor of Enemy Mod Script & Initial Boss Imp
    Refactor Enemy mod script to Character wide for use later; Initial Boss implementation.

---
+++ rpg-James_Doyle/Assets/Scripts/Mod Tools/CharMod.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharMod : MonoBehaviour
+{
+    public static CharMod instance;
+
+    [Header("Panels and Prefabs")]
+    public GameObject[] charPrefabs;
+
+    public GameObject[] charPanels;
+
+    [Header("Text Values")]
+    public Text[] charName;
+    public Text[]
+        charHP,
+        charMP,
+        charStr,
+        charDef,
+        wpnPwr,
+        armourPwr;
+    
+    public Image[] charImage;
+
+    [Header("Input Values")]
+    public InputField[] newHP;
+    public InputField[] newMP, newStr, newDef, newWpnPwr, newArmPwr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        //AutoFill(); //fill out stats
+    }
+
+    public void AutoFill()
+    {
+        for (int i = 0; i < charPanels.Length; i++)
+        {
+            //Debug.Log(i);
+
+            for (int j = 0; j < charPrefabs.Length; j++)
+            {
+                //Debug.Log(j);
+
+                int selectedChar = charPanels[i].gameObject.GetComponent<SelectChar>().selectChar;
+
+                if (j == selectedChar)
+                {
+                    charName[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().charName;
+                    charHP[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().currentHp.ToString();
+                    charMP[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().currentMp.ToString();
+                    charStr[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().strength.ToString();
+                    charDef[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().defence.ToString();
+                    wpnPwr[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().wpnPower.ToString();
+                    armourPwr[i].text = charPrefabs[selectedChar].GetComponent<BattleChar>().armPower.ToString();
+
+                    charImage[i].sprite = charPrefabs[selectedChar].GetComponent<SpriteRenderer>().sprite;
+                }
+            }
+        }
+    }
+
+    public void SaveInput()
+    {
+        for (int i = 0; i < charPrefabs.Length; i++)
+        {
+            //save button for User inputted values
+            if (!string.IsNullOrEmpty(newHP[i].text))
+            {
+                //handles any potential exceptions from poor user entered values
+                int.TryParse(newHP[i].text, out charPrefabs[i].GetComponent<BattleChar>().currentHp);
+            }
+
+            if (!string.IsNullOrEmpty(newMP[i].text))
+                int.TryParse(newMP[i].text, out charPrefabs[i].GetComponent<BattleChar>().currentMp);
+
+            if (!string.IsNullOrEmpty(newStr[i].text))
+                int.TryParse(newStr[i].text, out charPrefabs[i].GetComponent<BattleChar>().strength);
+
+            if (!string.IsNullOrEmpty(newDef[i].text))
+                int.TryParse(newDef[i].text, out charPrefabs[i].GetComponent<BattleChar>().defence);
+
+            if (!string.IsNullOrEmpty(newWpnPwr[i].text))
+                int.TryParse(newWpnPwr[i].text, out charPrefabs[i].GetComponent<BattleChar>().wpnPower);
+
+            if (!string.IsNullOrEmpty(newArmPwr[i].text))
+                int.TryParse(newArmPwr[i].text, out charPrefabs[i].GetComponent<BattleChar>().armPower);
+
+            AutoFill();
+        }
+    }
+}
--- rpg-James_Doyle/Assets/Scripts/ModButtons.cs
+++ rpg-James_Doyle/Assets/Scripts/ModButtons.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModButtons : MonoBehaviour
 {
+    private GameObject[] bossDragon;
+
+    public Image bossImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (bossDragon == null)
+        {
+            bossDragon = GameObject.FindGameObjectsWithTag("Boss");
+        }
+        //bossDragon = GameObject.FindGameObjectWithTag("Boss");
     }
 
     // Update is called once per frame
@@ -18,5 +27,34 @@ public class ModButtons : MonoBehaviour
     public void PlayButtonSound()
     {
         AudioManager.instance.PlaySFX(4);
+    }
+
+    public void CharEnable()
+    {
+        //activate/deactivate char
+        foreach (var boss in bossDragon)
+        {
+            boss.SetActive(true);
+
+        }
+
+        bossImage.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+        //bossDragon.SetActive(true);
+    }
+    public void CharDisable()
+    {
+        foreach (var boss in bossDragon)
+        {
+            boss.SetActive(false);
+        }
+
+        bossImage.GetComponent<Image>().color = new Color32(121, 121, 123,100);
+    }
+
+    public void SaveCharStats()
+    {
+        //calls func from mod script
+        CharMod.instance.SaveInput();
     }
 }
