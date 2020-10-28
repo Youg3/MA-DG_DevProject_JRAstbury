a70fef5b3536f64a59287ceb64b3a4eddc7be637
Joshua Astbury <j.astbury@ymail.com>
2020-10-28 17:58:02 +0100

    BZ Timers; Routes Un/Lock; World UI; Minimap Cam
    BZ Timers retrieve, load & save correctly. Implemented 3 routes for World Mod, correctly unlock/lock with toggles. World UI implementation. Minimap Camera and layer applied.

--- rpg-James_Doyle/Assets/Scripts/Mod Tools/WorldMod.cs
+++ rpg-James_Doyle/Assets/Scripts/Mod Tools/WorldMod.cs
@@ -9,11 +9,15 @@ public class WorldMod : MonoBehaviour
 
 
     public GameObject[] battleZones;
-    public GameObject[] bossEnemies;
+    public InputField[] bzTimers;
+    public GameObject[] pathBlockers;
+    public GameObject[] pathExits; //holds the two exit paths separate.
 
-    [Header("Text Values")] 
-    public Text battleTimer;
+    public Toggle route1;
+    public Toggle route2;
+    public Toggle route3;
 
+    public Toggle[] routeGroups;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +25,58 @@ public class WorldMod : MonoBehaviour
         instance = this;
 
         FillValues();
+
+        route1.onValueChanged.AddListener(delegate { OnToggleChange(0,1); });
+        route2.onValueChanged.AddListener(delegate { OnToggleChange(2,3); });
+        route3.onValueChanged.AddListener(delegate { OnToggleChange(4,5); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SaveWorldMod();
+            FillValues();
+        }
     }
 
     public void FillValues()
     {
-        //fill the open values with current
-        battleTimer.text = battleZones[0].GetComponent<BattleStarter>().pauseBetweenBattles.ToString();
+        //Fill Battle Zone Timers.
+        for (int i = 0; i <= bzTimers.Length -1; i++)
+        {
+            for (int j = 0; j <= battleZones.Length -1; j++)
+            {
+                BattleStarter currentZone = battleZones[i].GetComponent<BattleStarter>();
+                FillBZTimers(i,currentZone);
+            }
+        }
+        //get toggle values
 
+    }
+
+    public void SaveWorldMod()
+    {
+        // save Battle Zone Timers...
+        for (int i = 0; i <= bzTimers.Length - 1; i++)
+        {
+            for (int j = 0; j <= battleZones.Length - 1; j++)
+            {
+                BattleStarter currentZone = battleZones[i].GetComponent<BattleStarter>();
+                SaveBZTimers(i, currentZone);
+            }
+        }
+    }
+
+    public void FillBZTimers(int Inputfield, BattleStarter bzNum)
+    {
+        bzTimers[Inputfield].text = bzNum.pauseBetweenBattles.ToString();
+    }
+
+    public void SaveBZTimers(int Inputfield, BattleStarter bzNum)
+    {
+        float.TryParse(bzTimers[Inputfield].text, out bzNum.pauseBetweenBattles); //save entered value
     }
 
 
@@ -42,4 +85,91 @@ public class WorldMod : MonoBehaviour
         //activate/deactivate certain overworld objs.
     }
 
+    public void ExitPath()
+    {
+        if (route1.isOn)
+        {
+            pathExits[0].SetActive(!pathExits[0].activeInHierarchy);
+        }
+    }
+
+    public void OnToggleValueChanged(Toggle name)
+    {
+        Debug.Log(name);
+        Debug.Log(route1);
+        //exit logic
+        if (name == route1)
+        {
+            //unlock/lock road
+            pathBlockers[0].SetActive(!pathBlockers[0].activeInHierarchy);
+            pathBlockers[1].SetActive(!pathBlockers[1].activeInHierarchy);
+
+            //pathExits[0].SetActive(!pathExits[0].activeInHierarchy);
+
+        }
+        if (name == route2)
+        {
+            //unlock/lock road
+            pathBlockers[2].SetActive(!pathBlockers[2].activeInHierarchy);
+            pathBlockers[3].SetActive(!pathBlockers[3].activeInHierarchy);
+
+        }
+
+        if (name == route3)
+        {
+            //unlock/lock road
+            pathBlockers[4].SetActive(!pathBlockers[4].activeInHierarchy);
+            pathBlockers[5].SetActive(!pathBlockers[5].activeInHierarchy);
+
+        }
+        RouteThreeLogic();
+    }    
+    public void OnToggleChange(int blocker1, int blocker2)
+    {
+        //unlock/lock road
+        pathBlockers[blocker1].SetActive(!pathBlockers[blocker1].activeInHierarchy);
+        pathBlockers[blocker2].SetActive(!pathBlockers[blocker2].activeInHierarchy);
+
+        RouteThreeLogic(); //call the logic behind deciding which exit should be unlocked.
+    }
+
+    public void RouteThreeLogic()
+    {
+        if (route3.isOn)
+        {
+            if (!route1.isOn && route2.isOn)
+            {
+                pathExits[0].SetActive(false);
+                pathExits[1].SetActive(true);
+            }
+            else if (route1.isOn && !route2.isOn)
+            {
+                pathExits[0].SetActive(true);
+                pathExits[1].SetActive(false);
+            }
+            else if (route1.isOn && route2.isOn)
+            {
+                pathExits[0].SetActive(false);
+                pathExits[1].SetActive(false);
+            }
+        }
+        else if (!route3.isOn)
+        {
+            if (route1.isOn && route2.isOn)
+            {
+                pathExits[0].SetActive(false);
+                pathExits[1].SetActive(false);
+            }
+            else if (route1.isOn && !route2.isOn)
+            {
+                pathExits[0].SetActive(false);
+                pathExits[1].SetActive(true);
+            }
+            else if (!route1.isOn && route2.isOn)
+            {
+                pathExits[0].SetActive(true);
+                pathExits[1].SetActive(false);
+            }
+        }
+    }
 }
